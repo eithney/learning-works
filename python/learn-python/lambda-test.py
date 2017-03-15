5dba998722ea8f71f098e92431cecf44243d2553
eithney <eithney@163.com>
2017-03-15 14:53:33 +0800

    test-03.01

---
+++ python/learn-python/lambda-test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+temp = lambda x,y:x+y ** 2
+
+print temp(4,5)
---
+++ python/learn-python/test-03/module/demo.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+print 'this a demo file, can be included'
